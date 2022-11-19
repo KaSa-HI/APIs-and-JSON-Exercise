e74002ba26f6671f38cc8b0ffa4db5e3d474d285
KaSahi <kahvisabatino@gmail.com>
2022-11-18 21:13:32 -1000

    submit

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HttpClient client = new HttpClient();
+
+            string kanyeURL = string.Empty;
+            string kanyeResponse = string.Empty;
+            string kanyeQuote = string.Empty;
+
+            string ronURL = string.Empty;
+            string ronResponse = string.Empty; ;
+            string ronQuote = string.Empty;
+
+            for (int i = 0; i < 5; i++)
+            {
+                 kanyeURL = "https://api.kanye.rest";
+                 kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+                 kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+                //Console.WriteLine(kanyeResponse);
+                //Console.WriteLine(kanyeQuote);
+                Console.WriteLine($"Kanye says: \"{kanyeQuote}\"");
+
+
+
+                 ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+                 ronResponse = client.GetStringAsync(ronURL).Result;
+
+                 ronQuote = JArray.Parse(ronResponse).ToString().Replace("[", "").Replace("]", "").Trim();
+                //Console.WriteLine(ronResponse);
+                //Console.WriteLine(ronQuote);
+                Console.WriteLine($"Ron Swanson says: {ronQuote}");
+                Console.WriteLine();
+
+
+            }
         }
     }
 }
